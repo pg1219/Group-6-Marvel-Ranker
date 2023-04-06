cb8d8eccbfd1d7fb5965fcbaa438bdd24dc46b6b
pg1219 <peter.gronseth@gmail.com>
2023-04-05 21:45:23 -0400

    late push

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@
  var spanTag = document.getElementById('spanTags');
  var publicKey = "fa6f290698944f5999102cfe84473ee5";
  var privateKey = "c7f11873efae271d19170c35a78fe6009417f858";
+ var charIds = ["1010801", "1009187", "1017105", "1009282", "1017107", "1010338", "1009610", "1009664", "1009368", "1009189"]
  
  var ts = Date.now();
  var combined = ts + privateKey + publicKey;
@@ -27,7 +28,7 @@
        
         var charItem= document.createElement('h3');
         var charList =reply.data.results[i];
-         charItem.textContent= charList.name;;
+         charItem.textContent= charList.name;
          spanTag.append(charItem);
          console.log(reply.data.results)
  
@@ -37,7 +38,7 @@
        var imageList = reply.data.results[i];
  
        var imageItem = document.createElement('img');
-       imageItem.style.width= '100px';
+       imageItem.style.width= '75px';
        imageItem.src =  imageList.thumbnail.path  + "." + imageList.thumbnail.extension;
        
        charItem.append(imageItem);
@@ -51,3 +52,19 @@
  
 
  var marvelUrl = "https://api.giphy.com/v1/gifs/random?api_key=7saPLMk09bQDFKz96FN2CCcwFpfGlp84&tag=marvel&rating=g"
+
+
+
+
+
+
+// 1010801 Ant Man scott lang
+// 1009187 black panther
+// 1017105 capt america
+// 1009282 dr strange
+// 1017107 hulk
+// 1010338 capt marvel
+// 1009610 spiderman
+// 1009664 Thor
+// 1009368 iron man
+// 1009189 Black widow
