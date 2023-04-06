082c60dfe42e4b61c0019f7eb0a7bdf3e165158d
pg1219 <peter.gronseth@gmail.com>
2023-04-05 20:55:08 -0400

    push

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,3 +47,5 @@
    })
  }
  fetchCharsButton.addEventListener('click', getCharsApi)
+
+ var marvelUrl = "https://api.giphy.com/v1/gifs/random?api_key=7saPLMk09bQDFKz96FN2CCcwFpfGlp84&tag=marvel&rating=g"
