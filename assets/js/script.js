28486faed97bcdacfec8ddbf222e6ef607410102
pg1219 <peter.gronseth@gmail.com>
2023-04-09 22:35:12 -0400

    late sunday night push with 8pm meeting edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 var charTag = document.getElementById("charTags");
 var fetchCharsButton = document.getElementById("btn");
 var spanTag = document.getElementById("spanTags");
-var publicKey = "fa6f290698944f5999102cfe84473ee5";
-var privateKey = "c7f11873efae271d19170c35a78fe6009417f858";
+var publicKey = "475379ab26db81da4efbde3f762b8587";
+var privateKey = "256f300dba7897285cb040464d292143177ed5b9";
 var charIds = [
-  "1010802",
+  "1010801",
   "1009187",
   "1009220",
   "1009282",
@@ -40,17 +40,18 @@ function getCharsApi() {
       .then(function (reply) {
         console.log(reply);
 
-        var charItem = document.createElement("h3");
+        var charItem = document.createElement("ul");
         var charList = reply.data.results[0];
         charItem.textContent = charList.name;
-        console.log(charItem.textContent)
+        charItem.style.fontSize = "1vw";
+        console.log(charItem.textContent);
         spanTag.append(charItem);
 
-        console.log(charList)
+        console.log(charList);
 
         var imageList = reply.data.results[0];
         var imageItem = document.createElement("img");
-        imageItem.style.width = "75px";
+        imageItem.style.width = "100px";
         imageItem.src =
           imageList.thumbnail.path + "." + imageList.thumbnail.extension;
         imageItem.classList.add("char-img");
@@ -64,38 +65,47 @@ spanTag.addEventListener("click", function (event) {
   var element = event.target;
   if (element.matches(".char-img")) {
     console.log(element.dataset.name);
-    localStorage.setItem("name", element.dataset.name)
+    localStorage.setItem("name", element.dataset.name);
     fetch(
-      "https://api.giphy.com/v1/gifs/search?api_key=7saPLMk09bQDFKz96FN2CCcwFpfGlp84&q=" + element.dataset.name + "&rating=g&limit=10")
-
+      "https://api.giphy.com/v1/gifs/search?api_key=7saPLMk09bQDFKz96FN2CCcwFpfGlp84&q=" +
+        element.dataset.name +
+        "&rating=g&limit=10"
+    )
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
-        console.log(data)
-      
-      for (var i = 0; i < 1; i++){ 
-        var index = (Math.floor(Math.random() * data.data.length));
-      
-      console.log(index)
-      
-   var gifId = data.data[index].images.original.url
-   console.log(gifId)
-  var gifBox = document.querySelector(".gif-container")
-   var favImg = document.createElement("img");
-favImg.style.width = "700px";
-favImg.style.padding = "50px"
-favImg.src = gifId
-favImg.classList.add("char-gif");
-favImg.setAttribute("gif", gifId);
-gifBox.append(favImg);}
+        console.log(data);
 
+        for (var i = 0; i < 1; i++) {
+          var index = Math.floor(Math.random() * data.data.length);
 
+          console.log(index);
+
+          var gifId = data.data[index].images.original.url;
+          console.log(gifId);
+          var gifBox = document.querySelector(".gif-container");
+          var favImg = document.createElement("img");
+          favImg.style.width = "90%";
+          favImg.style.padding = "30px";
+          favImg.src = gifId;
+          favImg.classList.add("char-gif");
+          favImg.setAttribute("gif", gifId);
+          gifBox.append(favImg);
+
+          // if (gifId){
+          //   getCharsApi(gifId);
+          //   gifId.textContent = ""
+          //   gifId.value = ""  
+          // }else{
+          //   display = hidden
+          // }
+        }
+      });
+  }
 });
- } })
 
-
-fetchCharsButton.addEventListener("click", getCharsApi)
+fetchCharsButton.addEventListener("click", getCharsApi);
 
 const hideBtn = document.getElementById("btn");
 fetchCharsButton.onclick = function () {
@@ -103,4 +113,5 @@ fetchCharsButton.onclick = function () {
     hideBtn.style.display = "none";
   } else {
     hideBtn.style.display = "block";
-  }}
+  }
+};
